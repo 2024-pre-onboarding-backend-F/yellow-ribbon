372b1f052077aaf42acaa6da58200f137f057c68
Jinhui <jda0526@naver.com>
2024-08-28 12:29:24 +0900

    feat : 에러 처리 핸들링 세팅 파일 구현

---
+++ src/main/java/wanted/ribbon/exception/ForbiddenException.java
@@ -0,0 +1,12 @@
+package wanted.ribbon.exception;
+
+public class ForbiddenException extends BaseException {
+
+    public ForbiddenException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public ForbiddenException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+}
---
+++ src/main/java/wanted/ribbon/exception/InternalServerException.java
@@ -0,0 +1,12 @@
+package wanted.ribbon.exception;
+
+public class InternalServerException extends BaseException {
+
+    public InternalServerException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public InternalServerException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+}
