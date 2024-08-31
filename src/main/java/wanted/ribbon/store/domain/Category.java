9602441281ad84d29692d5b3476526afb21f6647
Jinhui <jda0526@naver.com>
2024-08-31 22:56:10 +0900

    refactor : Category 값인 '까페' → '일식', Gimbap → Lunch로 변경

--- src/main/java/wanted/ribbon/store/domain/Category.java
+++ src/main/java/wanted/ribbon/store/domain/Category.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    Gimbap("김밥(도시락)"), Cafe("까페"), Chinese("중국식");
+    Lunch("김밥(도시락)"), Japanese("일식"), Chinese("중국식");
 
     private final String name;
 }
