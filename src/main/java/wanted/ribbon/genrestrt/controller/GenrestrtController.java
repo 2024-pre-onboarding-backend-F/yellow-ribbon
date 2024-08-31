c83aa0eb9c24c71b227a636a245cff4512063091
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-01 00:49:16 +0900

    refactor : PostMapping으로 변경

--- src/main/java/wanted/ribbon/genrestrt/controller/GenrestrtController.java
+++ src/main/java/wanted/ribbon/genrestrt/controller/GenrestrtController.java
@@ -1,12 +1,9 @@
 package wanted.ribbon.genrestrt.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientException;
 import wanted.ribbon.genrestrt.service.GenrestrtService;
 
 @RestController
@@ -16,7 +13,7 @@ public class GenrestrtController {
     private final GenrestrtService genrestrtService;
 
     // 경기도 맛집 데이터 수집 API
-    @GetMapping("/fetch-data")
+    @PostMapping("/fetch-data")
     public ResponseEntity<String> fetchData(@RequestParam("serviceName") String serviceName) {
         // openAPI 호출
         genrestrtService.fetchAndSaveData(serviceName);
