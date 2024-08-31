f991e6e83980a8331c551e3396c2464a721f67d9
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-01 00:48:36 +0900

    refactor: Repository 어노테이션 삭제

--- src/main/java/wanted/ribbon/genrestrt/repository/GenrestrtRepository.java
+++ src/main/java/wanted/ribbon/genrestrt/repository/GenrestrtRepository.java
@@ -1,9 +1,7 @@
 package wanted.ribbon.genrestrt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import wanted.ribbon.genrestrt.domain.Genrestrt;
 
-@Repository
 public interface GenrestrtRepository extends JpaRepository<Genrestrt, Long> {
 }
