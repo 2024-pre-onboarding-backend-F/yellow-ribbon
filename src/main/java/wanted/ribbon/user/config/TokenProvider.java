47f638eb6bc2f446326629e6f67bce191adb6892
Youngjoo <tyty4420@naver.com>
2024-09-02 07:47:01 +0900

    refactor : 토큰 제공자 추가하는 기능

---
+++ src/main/java/wanted/ribbon/user/config/TokenProvider.java
@@ -0,0 +1,79 @@
+package wanted.ribbon.user.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import wanted.ribbon.user.config.jwt.JwtProperties;
+import wanted.ribbon.user.domain.User;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt){
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    private String makeToken(Date expiry, User user){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ : JWT
+                .setIssuer(jwtProperties.getIssuer()) // 내용 iss : properties 파일에서 설정한 값
+                .setIssuedAt(now) // 내용 iat : 현재 시간
+                .setExpiration(expiry) // 내용 exp : expiry 멤버 변수 값
+                .setSubject(user.getId()) // 내용 sub : 유저의 계정명
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    // JWT 토큰 유효성 검증 메서드
+    public boolean validToken(String token){
+        try{
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey()) // 비밀번호 복호화
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {// 복호화 과정에서 에러가 나면 유효하지 않은 토큰
+            return false;
+        }
+    }
+
+    // 토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication(String token){
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority(("ROLE_USER")));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities), token, authorities);
+    }
+
+    // 토큰 기반으로 유저 ID를 가져오는 메서드
+    public UUID getUserId(String token){
+        Claims claims = getClaims(token);
+        return claims.get("userId", UUID.class);
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser() // 클레임 조회
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+}
