da045c192fe701f71425ca8195f85222e5d318fd
Jinhui <jda0526@naver.com>
2024-09-01 23:22:25 +0900

    feat : 필수 @RequestParam이 입력되지 않았을 때 예외 처리하는 핸들러 구현

--- src/main/java/wanted/ribbon/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/wanted/ribbon/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package wanted.ribbon.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wanted.ribbon.exception.*;
@@ -14,6 +15,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getErrorCode(), e.getErrorCode().getMessage()));
     }
+
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ErrorResponse> handleConflictException(ConflictException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
@@ -36,5 +38,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(e.getErrorCode(), e.getErrorCode().getMessage()));
+    }
+
+    // 필수 @RequestParam이 입력되지 않았을 때
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException e) {
+        String paramName = e.getParameterName();
+        String errorMessage = "필수 파라미터 '" + paramName + "'가 누락되었습니다.";
+
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE, errorMessage));
     }
 }
