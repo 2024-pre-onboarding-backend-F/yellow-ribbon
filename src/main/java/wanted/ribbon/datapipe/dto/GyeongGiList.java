bef2f5ad0c64325c1b63ccd31aaf268e12fbe468
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-29 10:40:41 +0900

    feat: 경기도 맛집 원본 데이터 openApi 가져오기 기능 추가

---
+++ src/main/java/wanted/ribbon/datapipe/dto/GyeongGiList.java
@@ -0,0 +1,32 @@
+package wanted.ribbon.datapipe.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public record GyeongGiList(String message,
+                           Long total,
+                           List<GyeongGiApiResponse> gyeongGiApiResponses) {
+    public record GyeongGiApiResponse(String sigunNm,
+                                      String sigunCd,
+                                      String bizplcNm,
+                                      Date licensgDe,
+                                      String bsnStateNm,
+                                      Date clsbizDe,
+                                      Double locplcAr,
+                                      String gradFacltDivNm,
+                                      Long maleEnflpsnCnt,
+                                      Integer yy,
+                                      String multiUseBizestblYn,
+                                      String gradDivNm,
+                                      Double totFacltScale,
+                                      Long femaleEnflpsnCnt,
+                                      String bsnsiteCircumfrDivNm,
+                                      String sanittnIndutypeNm,
+                                      String sanittnBizcondNm,
+                                      Long totEmplyCnt,
+                                      String refineRoadnmAddr,
+                                      String refineLotnoAddr,
+                                      String refineZipCd,
+                                      Double refineWgs84Lat,
+                                      Double refineWgs84Logt){}
+}
