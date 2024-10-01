bd80232c97cc1d053270e088a4fbf488919da75e
jeongeungyeong <jeongeung1124@gmail.com>
2024-10-02 07:19:41 +0900

    feat: swaggerConfig 추가

---
+++ src/main/java/wanted/ribbon/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package wanted.ribbon.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder() //경로 설정
+                .group("springdoc-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Yellow-Ribbon API")
+                        .version("v2")
+                        .description("옐로리본 운영서버 API 명세서"));
+    }
+}
