49070788a81da2c223f0a0417ba73dff2dcd391b
LeeJiWon <dlwldnjs427@naver.com>
2024-10-02 15:04:46 +0900

    feat : 인기 맛집 조회 쿼리 작성

--- src/main/java/wanted/ribbon/store/repository/StoreRepository.java
+++ src/main/java/wanted/ribbon/store/repository/StoreRepository.java
@@ -1,10 +1,17 @@
 package wanted.ribbon.store.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import wanted.ribbon.store.domain.Store;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom  {
     Optional<Store> findByStoreId(Long StoreId);
+
+    @Query("SELECT s FROM Store s WHERE s.rating >= :rating AND s.reviewCount >= :reviewCount ORDER BY s.rating DESC, s.reviewCount DESC")
+    List<Store> findPopularStores(@Param("rating")double rating, @Param("reviewCount")Integer reviewCount, Pageable pageable);
 }
