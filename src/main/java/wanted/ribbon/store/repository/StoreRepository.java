60541fc0779ec78a79f19094dc543de60a426748
LeeJiWon <dlwldnjs427@naver.com>
2024-10-02 15:49:56 +0900

    feat : 카테고리별 인기 맛집 조회 쿼리 작성

--- src/main/java/wanted/ribbon/store/repository/StoreRepository.java
+++ src/main/java/wanted/ribbon/store/repository/StoreRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import wanted.ribbon.store.domain.Category;
 import wanted.ribbon.store.domain.Store;
 
 import java.util.List;
@@ -14,4 +15,7 @@ public interface StoreRepository extends JpaRepository<Store, Long>, StoreReposi
 
     @Query("SELECT s FROM Store s WHERE s.rating >= :rating AND s.reviewCount >= :reviewCount ORDER BY s.rating DESC, s.reviewCount DESC")
     List<Store> findPopularStores(@Param("rating")double rating, @Param("reviewCount")Integer reviewCount, Pageable pageable);
+
+    @Query("SELECT s FROM Store s WHERE s.category = :category AND s.rating >= :rating AND s.reviewCount >= :reviewCount ORDER BY s.rating DESC, s.reviewCount DESC")
+    List<Store> findPopularStoresByCategory(@Param("rating")double rating, @Param("reviewCount")Integer reviewCount, @Param("category") Category category, Pageable pageable);
 }
