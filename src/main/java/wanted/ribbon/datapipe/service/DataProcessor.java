d40d8a8c9e7fa6198f4794c5804fbc801b8bf037
jeongeungyeong <jeongeung1124@gmail.com>
2024-10-09 23:47:07 +0900

    refactor: 맛집 카테고리 별 케이스 추가

--- src/main/java/wanted/ribbon/datapipe/service/DataProcessor.java
+++ src/main/java/wanted/ribbon/datapipe/service/DataProcessor.java
@@ -22,21 +22,27 @@ public class DataProcessor implements ItemProcessor<RawData, Store> {
                 .reviewCount(0) // 리뷰수
                 .build();
     }
+
     // 카테고리명 이름 설정
-    private Category parseCategory(String category){
-        if (category == null || category.isEmpty()){
+    private Category parseCategory(String category) {
+        if (category == null || category.isEmpty()) {
             return Category.valueOf("카테고리명 정보 없음");
         }
-         // 실제 카테고리 값을 받아오는 경우, 카테고리 설정에 맞는지 확인하고 매핑
-        switch (category.trim()){
-            case "김밥(도시락)":
-                return Category.Lunch;
-            case "일식":
-                return Category.Japanese;
-            case "중국식":
-                return Category.Chinese;
-            default:
-                return Category.valueOf(category); // 입력받은 값 그대로 매핑
-        }
+        return switch (category.trim()) {
+            case "김밥(도시락)" -> Category.Lunch;
+            case "일식" -> Category.Japanese;
+            case "중국식" -> Category.Chinese;
+            case "까페" -> Category.Cafe;
+            case "이동조리" -> Category.Movmntcook;
+            case "탕류(보신용)" -> Category.Soup;
+            case "패스트푸드" -> Category.Fastfood;
+            case "횟집" -> Category.Sashimi;
+            case "뷔페식" -> Category.Buff;
+            case "복어취급" -> Category.Fugu;
+            case "정종/대포집/소주방" -> Category.Pub;
+            case "출장조리" -> Category.Bsrpcook;
+            case "전통찻집" -> Category.Tratearm;
+            default -> Category.ETC;
+        };
     }
 }
