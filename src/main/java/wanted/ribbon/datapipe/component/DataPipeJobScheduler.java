7543cebfbe500107c3e5ead7da9295c5a41b4bf3
jeongeungyeong <jeongeung1124@gmail.com>
2024-10-09 21:32:35 +0900

    feat: job 자동화 기능 추가

--- src/main/java/wanted/ribbon/datapipe/component/DataPipeJobScheduler.java
+++ src/main/java/wanted/ribbon/datapipe/component/DataPipeJobScheduler.java
@@ -1,28 +1,46 @@
 package wanted.ribbon.datapipe.component;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import wanted.ribbon.datapipe.config.DataPipeJobConfig;
 
 @Component
 @RequiredArgsConstructor
+@EnableScheduling
+@Slf4j
 public class DataPipeJobScheduler {
     private final JobLauncher jobLauncher;
-    private final DataPipeJobConfig dataPipeJobConfig;
+    private final Job openApiJob;
+    private final Job dataPipeJob;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void collectRawData(){
+        runJob(openApiJob,"공공데이터 수집");
+    }
 
     @Scheduled(cron = "0 0 3 * * 1") // 매주 월요일 새벽 3시 1번 cron 표현식 활용 (초 분 시 일 월 요일)
-    public void DataUpdate() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException, JobRestartException {
-        jobLauncher.run(
-                dataPipeJobConfig.dataPipeJob(),
-                new JobParametersBuilder()
-                        .toJobParameters()
-        );
+    public void processedDataUpdate(){
+        runJob(dataPipeJob,"운영 데이터 전처리");
+    }
+
+    private void runJob(Job job, String jobDescription) {
+        log.info("{} Job을 실행합니다.",jobDescription);
+        try {
+            // 각 실행마다 고유한 JobParameters 생성
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            // 작업 실행
+            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+            // 작업 실행 결과
+            log.info(" {} Job 결과입니다: {}", jobDescription,jobExecution.getStatus());
+        } catch (Exception e) {
+            // 예외 발생 시
+            log.error("{} Job에 예외가 발생했습니다: {}",jobDescription, e.getMessage());
+        }
     }
 }
