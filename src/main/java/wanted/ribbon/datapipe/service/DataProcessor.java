315107fa863e1d951db0dc830b91f8dec9a33559
jeongeungyeong <jeongeung1124@gmail.com>
2024-10-09 22:26:02 +0900

    refactor: store 리뷰개수 필드 관련 코드 수정

--- src/main/java/wanted/ribbon/datapipe/service/DataProcessor.java
+++ src/main/java/wanted/ribbon/datapipe/service/DataProcessor.java
@@ -10,7 +10,7 @@ import wanted.ribbon.store.domain.Store;
 public class DataProcessor implements ItemProcessor<RawData, Store> {
 
     @Override
-    public Store process(final RawData rawData) throws Exception {
+    public Store process(final RawData rawData) {
         return Store.builder()
                 .sigun(rawData.getSigunNm() != null ? rawData.getSigunNm() : "시군명 정보 없음")
                 .storeName(rawData.getBizplcNm() != null ? rawData.getBizplcNm() : "업장명 정보 없음")
@@ -18,7 +18,8 @@ public class DataProcessor implements ItemProcessor<RawData, Store> {
                 .address(rawData.getRefineRoadnmAddr() != null ? rawData.getRefineRoadnmAddr() : "도로명주소 정보 없음")
                 .storeLat(rawData.getRefineWgs84Lat() != null ? rawData.getRefineWgs84Lat() : 0.0)
                 .storeLon(rawData.getRefineWgs84Logt() != null ? rawData.getRefineWgs84Logt() : 0.0)
-                .rating(0.0)
+                .rating(0.0) // 가게 평점
+                .reviewCount(0) // 리뷰수
                 .build();
     }
     // 카테고리명 이름 설정
