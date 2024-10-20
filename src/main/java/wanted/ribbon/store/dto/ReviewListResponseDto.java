9b1535f107a64735066d3fc94d761f0dfef7a39b
LeeJiWon <dlwldnjs427@naver.com>
2024-10-20 22:17:19 +0900

    docs : swagger 명세서 추가 - 맛집 상세 정보 조회 응답 DTO 명세서 작성

--- src/main/java/wanted/ribbon/store/dto/ReviewListResponseDto.java
+++ src/main/java/wanted/ribbon/store/dto/ReviewListResponseDto.java
@@ -1,4 +1,12 @@
 package wanted.ribbon.store.dto;
 
-public record ReviewListResponseDto(String id, int score, String content) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "평가 목록 조회 응답 DTO",
+        example = "[{\"id\":\"1\",\"score\":5,\"content\":\"아주 좋았어요\"}," +
+                "{\"id\":\"2\",\"score\":4,\"content\":\"좋았어요\"}]")
+public record ReviewListResponseDto(
+        @Schema(description = "평가 식별번호") String id,
+        @Schema(description = "평가 점수") int score,
+        @Schema(description = "평가 내용") String content) {
 }
