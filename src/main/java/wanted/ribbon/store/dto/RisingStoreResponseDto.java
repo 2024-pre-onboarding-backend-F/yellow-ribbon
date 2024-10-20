e93ad0ed74ee01b7b0d7c01256f9239b3590849c
LeeJiWon <dlwldnjs427@naver.com>
2024-10-20 22:48:08 +0900

    docs : swagger 명세서 추가 - 인기 급상승 맛집 조회 응답 DTO 명세서 작성

--- src/main/java/wanted/ribbon/store/dto/RisingStoreResponseDto.java
+++ src/main/java/wanted/ribbon/store/dto/RisingStoreResponseDto.java
@@ -1,12 +1,22 @@
 package wanted.ribbon.store.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import wanted.ribbon.store.domain.Category;
 import wanted.ribbon.store.domain.Store;
 
-public record RisingStoreResponseDto(Long storeId, String sigun, String storeName,
-                                     Category category, String address,
-                                     double storeLat, double storeLon, double rating,
-                                     int rank) {
+@Schema(title = "인기 급상승 맛집 목록 응답 DTO",
+        example = "[{\"storeId\":1,\"sigun\":\"가평군\",\"storeName\":\"한솥도시락가평점\",\"category\":\"Lunch\",\"address\":\"경기도 가평군 가평읍 가화로 88-1, 1층\",\"storeLat\":37.8276878786,\"storeLon\":127.5147837255,\"rating\":4.5,\"rank\":1}," +
+                "{\"storeId\":2,\"sigun\":\"가평군\",\"storeName\":\"가평엄마손도시락\",\"category\":\"Lunch\",\"address\":\"경기도 가평군 가평읍 호반로 2605, 풀하우스 주1동 1층\",\"storeLat\":37.8184314186,\"storeLon\":127.5142365074,\"rating\":4.4,\"rank\":2}]")
+public record RisingStoreResponseDto(
+        @Schema(description = "맛집 식별번호") Long storeId,
+        @Schema(description = "시군명") String sigun,
+        @Schema(description = "사업장명") String storeName,
+        @Schema(description = "위생업태명(분류)") Category category,
+        @Schema(description = "소재지도로명주소") String address,
+        @Schema(description = "위도") double storeLat,
+        @Schema(description = "경도") double storeLon,
+        @Schema(description = "평점") double rating,
+        @Schema(description = "순위") int rank) {
     public static RisingStoreResponseDto from(Store store, int rank) {
         return new RisingStoreResponseDto(
                 store.getStoreId(),
